f9fe4994f5aa95deb3acc44afb11fe46fe14bcbe
Yuri Nazarenko <m@rezident.org>
2023-01-13 22:28:01 +0300

    Добавить cs fixer для php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+// https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/config.rst
+// https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/index.rst
+
+$finder = PhpCsFixer\Finder::create()->exclude('vendor');
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setUsingCache(false)
+    ->setRiskyAllowed(true)
+    ->setRules(
+        [
+            '@Symfony' => true,
+            '@Symfony:risky' => true,
+            '@PHP80Migration' => true,
+            '@PHP80Migration:risky' => true,
+            'concat_space' => ['spacing' => 'one'],
+        ]
+    )
+    ->setHideProgress(false)
+    ->setFinder($finder);
