68278dc9e51db06f702ea909d512ea7ba8083bad
Yuri Nazarenko <rezident@yandex-team.ru>
2023-02-15 18:08:58 +0300

    Добавить калькулятор смещения при запросе следующего обновления

---
+++ tests/update/UpdateOffsetCalculatorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rezident\WiseTelegramBot\tests\update;
+
+use Rezident\SelfDocumentedTelegramBotSdk\types\GettingUpdates\Update;
+use Rezident\WiseTelegramBot\tests\base\TestCase;
+
+class UpdateOffsetCalculatorTest extends TestCase
+{
+    public function testNullOffset(): void
+    {
+        $this->assertNull($this->container->get(UpdateOffsetCalculator::class)->getOffset());
+    }
+
+    public function testNextOffset(): void
+    {
+        $calculator = $this->container->get(UpdateOffsetCalculator::class);
+        $update = Update::fromArray(include __DIR__ . '/stub/update.php');
+        $calculator->calculate($update);
+        $this->assertEquals($update->getUpdateId() + 1, $calculator->getOffset());
+    }
+}
