b11018995a60410a239f25151631ae5dc95eb3b2
Yuri Nazarenko <rezident@yandex-team.ru>
2023-02-16 13:11:18 +0300

    Добавить использование калькулятора смещения для отслеживателя обновлений и тесты на параметра запросы

--- src/update/UpdatesWatcher.php
+++ src/update/UpdatesWatcher.php
@@ -6,19 +6,29 @@ namespace Rezident\WiseTelegramBot\update;
 
 use Rezident\SelfDocumentedTelegramBotSdk\components\Executor;
 use Rezident\SelfDocumentedTelegramBotSdk\methods\GettingUpdates\GetUpdatesMethod;
+use Rezident\WiseTelegramBot\tests\update\UpdateOffsetCalculator;
 
 class UpdatesWatcher
 {
-    public function __construct(private Executor $executor, private UpdateHandler $updateHandler)
-    {
+    private const TIMEOUT = 30;
+
+    public function __construct(
+        private Executor $executor,
+        private UpdateHandler $updateHandler,
+        private UpdateOffsetCalculator $updateOffsetCalculator,
+    ) {
     }
 
     public function watch(?int $requestsLimit = null): void
     {
         while (null === $requestsLimit || $requestsLimit--) {
-            $updates = GetUpdatesMethod::new()->exec($this->executor);
+            $updates = GetUpdatesMethod::new()
+                ->offset($this->updateOffsetCalculator->getOffset())
+                ->timeout(self::TIMEOUT)
+                ->exec($this->executor);
             foreach ($updates as $update) {
                 $this->updateHandler->handle($update);
+                $this->updateOffsetCalculator->calculate($update);
             }
         }
     }
