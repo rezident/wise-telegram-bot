093e80900cbdc4143e7284bbbfef7ba1ef1ce9da
Yuri Nazarenko <rezident@yandex-team.ru>
2023-02-17 18:27:14 +0300

    Добавить реализацию тестового бота

--- bootstrap.php
+++ bootstrap.php
@@ -2,4 +2,14 @@
 
 declare(strict_types=1);
 
-require_once 'vendor/autoload.php';
+use Rezident\WiseTelegramBot\Bot;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+function createBot(): Bot
+{
+    $bot = new Bot('5983262266:AAHvG06JTXaQ4o-zvTWSeez2kh-Wzxx0g9w');
+    $bot->readCommands(__DIR__ . '/bot/commands');
+
+    return $bot;
+}
