8d5ba30dfaf7c2d28032cc559e0dde3ac78c9213
Yuri Nazarenko <m@rezident.org>
2023-02-18 17:42:42 +0300

    Добавить в тестового бота ответ на start

--- bot/run.php
+++ bot/run.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$bot = createBot();
+createBot()->run();
---
+++ src/temp/commands/StartCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rezident\WiseTelegramBot\temp\commands;
+
+use Rezident\SelfDocumentedTelegramBotSdk\components\Executor;
+use Rezident\SelfDocumentedTelegramBotSdk\methods\SendMessageMethod;
+use Rezident\SelfDocumentedTelegramBotSdk\types\GettingUpdates\Update;
+use Rezident\WiseTelegramBot\command\CommandInterface;
+
+class StartCommand implements CommandInterface
+{
+    public function __construct(private Executor $executor)
+    {
+    }
+
+    public function handle(string $argument, Update $update): void
+    {
+        SendMessageMethod::new($update->getMessage()->getSenderChat()->getId(), 'привет')->exec($this->executor);
+    }
+}
