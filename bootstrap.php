dbcb2429ce350030662fb563067fce4055f783bf
Yuri Nazarenko <m@rezident.org>
2023-02-19 21:22:06 +0300

    Удалить тестового бота

--- bootstrap.php
+++ bootstrap.php
@@ -2,14 +2,4 @@
 
 declare(strict_types=1);
 
-use Rezident\WiseTelegramBot\Bot;
-
 require_once __DIR__ . '/vendor/autoload.php';
-
-function createBot(): Bot
-{
-    $bot = new Bot('5983262266:AAHvG06JTXaQ4o-zvTWSeez2kh-Wzxx0g9w');
-    $bot->readCommands(__DIR__ . '/src/temp/commands')->acceptOnly(85367147);
-
-    return $bot;
-}
